e17515349d5d31f239cf06b538efb604d05c8c54
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-04-20 16:14:34 +0200

    nb vector displayed

--- src/dispColumnMean.m
+++ src/dispColumnMean.m
@@ -1,2 +1,12 @@
-function dispColumnMean( title, matrixVector )
+function res = dispColumnMean( title, m1, m2, m3 )
+
+    m = zeros([1,3]);
+    
+    [~,m(1)] = size( m1 );
+    [~,m(2)] = size( m2 );
+    [~,m(3)] = size( m3 );
+    
+    res = mean( m );
+    
+    disp( strcat( title, ' : ', num2str( res ) ) );
 end
